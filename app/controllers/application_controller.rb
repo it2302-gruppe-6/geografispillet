ab568c4fa5e63a07eafeecef0aa19b54275e56a4
Lars Tobias <larstobi@stud.ntnu.no>
2011-04-10 14:58:29 +0200

    Added #index method and view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
   protect_from_forgery
+
+  # GET /
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
 end
