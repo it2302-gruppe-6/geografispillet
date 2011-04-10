faa629c309e0d349f16ad41658bd82f91cfa7008
Lars Tobias <larstobi@stud.ntnu.no>
2011-04-10 19:16:24 +0200

    Added Facebook app ID and secret.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-    provider :facebook, 'APP_ID', 'APP_SECRET'
+    provider :facebook, '191830237526889', '6e3d8bb629d7c2bed79028ea109cf1a2'
     # Mention other providers here you want to allow user to sign in with.
 end
