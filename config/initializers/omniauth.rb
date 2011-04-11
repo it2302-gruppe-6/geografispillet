fba1f180476849d47a4b8c4420a7d440bde1c57d
Lars Tobias <larstobi@stud.ntnu.no>
2011-04-11 11:50:56 +0200

    Removed monkeypatch for Faraday. Added OpenSSL VERIFY_NONE.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,12 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, '191830237526889', '6e3d8bb629d7c2bed79028ea109cf1a2'
 end
 
-require 'faraday'
-module Faraday 
-  class Adapter < Middleware
-    def call(env)
-      env[:ssl][:verify] = false if env[:ssl]
-      process_body_for_request(env)
-    end
-  end
-end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
