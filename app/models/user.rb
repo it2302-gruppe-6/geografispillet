f7dc37d8c708d36d850e7a4d8b005206cf051589
Lars Tobias <larstobi@stud.ntnu.no>
2011-04-11 14:14:34 +0200

    Indent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
+    acts_as_authentic
 
-  has_many :authentications
+    has_many :authentications
 
     def apply_omniauth(omniauth)
         self.email = omniauth['user_info']['email']
