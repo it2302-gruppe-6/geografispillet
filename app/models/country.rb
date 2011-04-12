a9b8b32a714eb9977b4acc44a56c5b0e734a2a56
Lars Tobias <larstobi@stud.ntnu.no>
2011-04-12 23:03:40 +0200

    acts_as_authentic on country, question and product.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,5 @@
 class Country < ActiveRecord::Base
+  acts_as_authentic
   attr_accessible :name, :population, :area, :currency, :capital
 
   has_many :questions
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
+  acts_as_authentic
   attr_accessible :question, :answer
 
   belongs_to :country
