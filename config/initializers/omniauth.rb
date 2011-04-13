19b4412ae967ed471c7bf0d9a3ba1fd7e013e6c1
Lars Tobias <larstobi@stud.ntnu.no>
2011-04-13 20:46:48 +0200

    Remove OpenSSL::SSL::VERIFY_NONE

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, '191830237526889', '6e3d8bb629d7c2bed79028ea109cf1a2'
 end
-
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
